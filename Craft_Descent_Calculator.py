1c299ae8dc70d72408594c1fb608607445b379bf
aero-error <57825679+aero-error@users.noreply.github.com>
2020-06-24 17:31:20 -0500

    Update Craft_Descent_Calculator.py

--- Craft_Descent_Calculator.py
+++ Craft_Descent_Calculator.py
@@ -35,22 +35,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ########## INPUTS ##########
-#Constants
-G = 6.67408e-11 #Gravitaional constant
+altitude = 350e3 #Starting altitude in M #400e3
+V0 = 2.42e3 #Inital velocity in M/S
+angle = 10 #entry angle in degrees (Below horizontal)
+
+
+Deploy1 = 30000 #altitude to deploy parachute
+Deploy2 = 2000  #altitude to stage parachute drop heat shield
+
 
 #Properties of the spacecraft
-CraftMass = 100 #Mass in KG
-CoD = 1.4       #Coefficient of drag
-CoL = 0.2       #Coefficent of lift
-CrossArea = 0.4 #Cross-sectional area of spacecraft M^2
 
-altitude = 400e3 #Starting altitude in M #400e3
-V0 = 2.38e3 #Inital velocity in M/S
-angle = 1 #entry angle in degrees (Below horizontal)
+### STAGE 1 ###
+CraftMass1 = 150 #Mass in KG
+CoD1 = 1.1       #Coefficient of drag
+CoL1 = 0.5       #Coefficent of lift
+CrossArea1 = 0.4 #Cross-sectional area of spacecraft M^2
+
+### STAGE 2 ###
+CraftMass2 = 150 #Mass in KG
+CoD2 = 1.6       #Coefficient of drag
+CoL2 = 0.64       #Coefficent of lift
+CrossArea2 = 1.4 #Cross-sectional area of spacecraft M^2
+
+### STAGE 3 ###
+CraftMass3 = 135 #Mass in KG
+CoD3 = 1.75       #Coefficient of drag
+CoL3 = 0.8       #Coefficent of lift
+CrossArea3 = 3.14 #Cross-sectional area of spacecraft M^2
 
 #Properties of Planet (Mars)
 MarsMass = 6.39e23 #Mass in KG
 MarsRadius = 3.3895e6 #Radius in M
+
+#Constants
+G = 6.67408e-11 #Gravitaional constant
 
 ########## FUNCTIONS ##########
 def Gravity(altitude): #returns acceleration due to gravity in meters/sec (EQNS REF:2)
@@ -65,9 +84,15 @@ def PressAtm(altitude): #returns atmospheric pressure in K-Pascals (EQNS REF:1)
     else:     
         T = -31 - 0.000998*altitude
         P = 0.699 * np.exp(-0.00009*altitude)
-    Density = P / (0.1921 * (T + 273.15))
+    
+    denominator = (0.1921 * (T + 273.15)) #note: this is a rough patch to fix density issiue 
+    if denominator < 10 and altitude > 100000:
+        denominator = 100
+    Density = P / denominator
+    
     if T < -273.1:
         T = -273.1
+        
     if Density < 0:
         Density = 1e-10
     return P, T, Density
@@ -95,12 +120,26 @@ gamma = angle
 v = V0
 dt = 0.01
 B = altitude
-m = CraftMass
-S = CrossArea
 r = MarsRadius
 h = altitude
 t = 0
 while h > 0:
+    if h > Deploy1:
+        S = CrossArea1
+        CoL = CoL1
+        CoD = CoD1
+        m = CraftMass1
+    elif h > Deploy2 and h <= Deploy1:
+        S = CrossArea2
+        CoL = CoL2
+        CoD = CoD2
+        m = CraftMass2
+    elif h <= Deploy2:
+        S = CrossArea3
+        CoL = CoL3
+        CoD = CoD3
+        m = CraftMass3
+        
     y = np.exp(-h/B)
     g = Gravity(h)
     p, T, rho = PressAtm(h)
@@ -131,28 +170,28 @@ print("RESULTS:")
 print()    
 print("The descent will take %.2f seconds" %(t))
 print("The craft will travel %.2f meters horizontally." %(dist_horiz))
+print("The craft will impact Mars at %.2f m/s." %(VEL[len(VEL)-1]))
 
 plt.figure(1)
-plt.scatter(TIME, VEL)
+plt.plot(TIME, VEL, "-k")
 plt.title("Velocity of SpaceCraft")
 plt.ylabel("Velocity (Meters/Sec)")
 plt.xlabel("Time (Sec)")
 
 plt.figure(2)
-plt.scatter(TIME, ANG)
+plt.plot(TIME, ANG, "-k")
 plt.title("Angle from Horizontal of Nominal Orbit")
 plt.ylabel("Angle (Degrees)")
 plt.xlabel("Time (Sec)")
 
 plt.figure(3)
-plt.scatter(TIME, ALT)
+plt.plot(TIME, ALT, "-k")
 plt.title("Altitude of SpaceCraft")
 plt.ylabel("Altitude (Meters)")
 plt.xlabel("Time (Sec)")    
 
 plt.figure(4)
-plt.scatter(TIME, Gs)
+plt.plot(TIME, Gs, "-r")
 plt.title("Deceleration of Spacecraft")
 plt.ylabel("Deceleration (Earth Gs)")
 plt.xlabel("Time (Sec)")
-
